ecb35d811fbf0e0ef77e85385b6c6572a53ecd4c
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2023-12-18 10:10:48 -0300

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -3,7 +3,7 @@ import win32com.client as win32
 outlook = win32.Dispatch('outlook.application') # É necessário ter o Outlook baixado e vínculado a uma conta, pois ele será utilizado para enviar as mensagens
 
 mail = outlook.CreateItem(0)
-mail.To =  '' #Inserir o Destinátario
+mail.To =  '' #Inserir o Destinátario - Obs: Você consegue mandar para mais de uma pessoa, no seguinte modelo: mail.To =  '1@mail.com; 2@mail.com; 3@mail;com' -> Todos separados por ';'
 mail.Subject = '' # Assunto do E-mail
 
 # As aspas triplas ''' indica que todo o conteúdo dentro dela é um HTML, pode usar as tags e styles
